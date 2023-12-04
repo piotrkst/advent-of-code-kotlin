07326c1eff5e6b1455fe182e8e08412e79008ffb
Kostek <piotr.kst@gmail.com>
2023-12-04 17:30:28 +0100

    d01 - Trebuchet.kt
    The second part has killed me for a long time :O

---
+++ src/y2023/d01/Trebuchet.kt
@@ -0,0 +1,64 @@
+package y2023.d01
+
+import readInput
+import kotlin.math.min
+
+private const val expectedTestAnswerPart1 = 142
+private const val expectedTestAnswerPart2 = 281
+
+private const val expectedAnswerPart1 = 54159
+private const val expectedAnswerPart2 = 53866
+
+private const val timeElapsedForPart1 = 8
+private const val timeElapsedForPart2 = 119
+
+
+fun main() {
+    val input = readInput(yearPackageName = "y2023", dayPackageName = "d01", fileName = "input")
+
+    println(Trebuchet.part1(input))
+    println(Trebuchet.part2(input))
+}
+
+object Trebuchet {
+    fun part1(input: List<String>) = input.mapNotNull {
+        val firstDigit = it.first { char -> char.isDigit() }
+        val lastDigit = it.last { char -> char.isDigit() }
+        "$firstDigit$lastDigit".toIntOrNull()
+    }.sum()
+
+    fun part2(input: List<String>) = input.mapNotNull {
+        val firstDigit = it.firstNumber
+        val lastDigit = it.lastNumber
+        "$firstDigit$lastDigit".toIntOrNull()
+    }.sum()
+
+    private val String.firstNumber
+        get() = findNumber(isInputReversed = false)
+
+    private val String.lastNumber
+        get() = with(reversed()) {
+            findNumber(isInputReversed = true)
+        }
+
+    private fun String.findNumber(isInputReversed: Boolean) = withIndex().firstNotNullOfOrNull { (index, char) ->
+        if (char.isDigit()) char.digitToInt()
+        else numbers.firstNotNullOfOrNull { number ->
+            val substring = substring(index, min(index + number.key.length, length))
+            val key = if (isInputReversed) number.key.reversed() else number.key
+            if (key == substring) number.value else null
+        }
+    }
+
+    private val numbers = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
+}
