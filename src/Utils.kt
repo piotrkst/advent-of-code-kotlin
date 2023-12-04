8b383e3930ed8bc0675a2b3632fb11d85acea849
Kostek <piotr.kst@gmail.com>
2023-12-04 18:52:05 +0100

    [MAINTENANCE] Removed unused util methods.

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,5 +1,3 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
@@ -8,15 +6,3 @@ import kotlin.io.path.readLines
  */
 fun readInput(yearPackageName: String, dayPackageName: String, fileName: String) =
     Path("src/$yearPackageName/$dayPackageName/$fileName.txt").readLines()
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
-
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
