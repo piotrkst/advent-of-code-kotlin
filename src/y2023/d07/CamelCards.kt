8ee022573a732b50dc5bf9df7f3c259317628610
Piotr Kostecki <piotr.kostecki@mohemian.com>
2023-12-07 14:38:32 +0100

    [MAINTENANCE] d07 - Removed ugly conditional checks in favor of rule definition.

--- src/y2023/d07/CamelCards.kt
+++ src/y2023/d07/CamelCards.kt
@@ -14,6 +14,7 @@ import y2023.PuzzleAnswer
 fun main() {
     CamelCards.checkParts<Int, Int>()
 }
+
 object CamelCards : PuzzleAnswer<Int, Int> {
     override val day = 7
     override val expectedTestAnswerPart1 = 6440
@@ -42,11 +43,7 @@ object CamelCards : PuzzleAnswer<Int, Int> {
         .sortedBy { it.hand }
         .foldIndexed(0) { index, acc, item -> acc.accumulate(index, item) }
 
-    private fun Int.accumulate(index: Int, input: Input): Int {
-        val winning = ((index + 1) * input.bid)
-        println("ID: $index, hand: ${input.hand.cards}, ${input.hand.type}, bid: ${input.bid}, winning: $winning")
-        return this + winning
-    }
+    private fun Int.accumulate(index: Int, input: Input) = this + ((index + 1) * input.bid)
 
     data class Input(
         val hand: Hand,
@@ -59,8 +56,8 @@ object CamelCards : PuzzleAnswer<Int, Int> {
     ): Comparable<Hand> {
         override fun compareTo(other: Hand) = isStrongerByType(this, other)
 
-        val cards: List<Int> = input.mapToCards()
-        val type: HandType = cards.handType()
+        private val cards: List<Int> = input.mapToCards()
+        private val type: HandType = cards.handType()
 
         private fun List<Char>.mapToCards() = map {
             when (it) {
@@ -74,35 +71,19 @@ object CamelCards : PuzzleAnswer<Int, Int> {
         }
 
         private fun List<Int>.handType(): HandType {
-            val cardsCounter = IntArray(14) { _ -> 0 }
-            forEach { card -> cardsCounter[card - 1]++ }
-            var max = cardsCounter.drop(1).max()
-            val indexOfMax = cardsCounter.drop(1).indexOf(max) + 1
+            val cardsCounter = IntArray(14) { _ -> 0 } // all possible values counter (1-14)
+            forEach { card -> cardsCounter[card - 1]++ } // fill matrix
+            val max = cardsCounter.drop(1).max() // find max excluding joker
+            val indexOfMax = cardsCounter.drop(1).indexOf(max) + 1 // get index of max excluding joker
             if (isJokerRuleApplied) {
-                cardsCounter[indexOfMax] += cardsCounter[0]
-                cardsCounter[0] = 0
+                cardsCounter[indexOfMax] += cardsCounter[0] // bump max with jokers
+                cardsCounter[0] = 0 // remove joker from matrix
             }
-            max = cardsCounter.max()
-            val firstSet = max.toHandType() ?: error("Invalid input")
-            if (firstSet in listOf(HandType.FIVE_OF_A_KIND, HandType.FOUR_OF_A_KIND)) return firstSet
-            cardsCounter[indexOfMax] = 0
-            val secondSet = cardsCounter.max().toHandType()
-            if (firstSet.repeatedCards != secondSet?.repeatedCards
-                    && setOf(
-                            firstSet.repeatedCards,
-                            secondSet?.repeatedCards,
-                    ).containsAll(setOf(2, 3))) return HandType.FULL_HOUSE
-            return when {
-                firstSet.repeatedCards == HandType.THREE_OF_A_KIND.repeatedCards -> HandType.THREE_OF_A_KIND
-                firstSet.repeatedCards == HandType.TWO_PAIR.repeatedCards
-                        && secondSet?.repeatedCards == HandType.TWO_PAIR.repeatedCards -> HandType.TWO_PAIR
-                firstSet.repeatedCards == HandType.ONE_PAIR.repeatedCards -> HandType.ONE_PAIR
-                else -> HandType.HIGH_CARD
-            }
+            return cardsCounter.filterNot { it == 0 }.toHandType() ?: error("Undefined")
         }
 
-        private fun Int.toHandType() = HandType.entries.find {
-            it.repeatedCards == this
+        private fun ListWithoutEmptyEntries.toHandType() = HandType.entries.find {
+            it.rule(sortedDescending())
         }
 
         private fun isStrongerByType(first: Hand, second: Hand) = when {
@@ -125,13 +106,19 @@ object CamelCards : PuzzleAnswer<Int, Int> {
         }
     }
 
-    enum class HandType(val strength: Int, val repeatedCards: Int? = null) {
-        FIVE_OF_A_KIND(strength = 7, repeatedCards = 5),
-        FOUR_OF_A_KIND(strength = 6, repeatedCards = 4),
-        FULL_HOUSE(strength = 5),
-        THREE_OF_A_KIND(strength = 4, repeatedCards = 3),
-        TWO_PAIR(strength = 3),
-        ONE_PAIR(strength = 2, repeatedCards = 2),
-        HIGH_CARD(strength = 1, repeatedCards = 1),
+    enum class HandType(val strength: Int, val rule: (EntriesSortedDescending) -> Boolean) {
+        FIVE_OF_A_KIND(strength = 7, rule = { entries -> entries.size == 1 }),
+        FOUR_OF_A_KIND(strength = 6, rule = { entries -> entries.size == 2 && entries.isExactly(listOf(4, 1)) }),
+        FULL_HOUSE(strength = 5, rule = { entries -> entries.size == 2 && entries.isExactly(listOf(3, 2)) }),
+        THREE_OF_A_KIND(strength = 4, rule = { entries -> entries.size == 3 && entries.isExactly(listOf(3, 1, 1)) }),
+        TWO_PAIR(strength = 3, rule = { entries -> entries.size == 3 && entries.isExactly(listOf(2, 2, 1)) }),
+        ONE_PAIR(strength = 2, rule = { entries -> entries.size == 4 }),
+        HIGH_CARD(strength = 1, rule = { entries -> entries.size == 5 });
+
     }
+
+    private fun List<Int>.isExactly(other: List<Int>) = zip(other).all { (first, second) -> first == second }
 }
+
+private typealias ListWithoutEmptyEntries = List<Int>
+private typealias EntriesSortedDescending = List<Int>
