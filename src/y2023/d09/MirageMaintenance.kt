240d804f52bc0afb68fb6aa2954c72b39c6b2ed2
Piotr Kostecki <piotr.kostecki@mohemian.com>
2023-12-11 17:28:18 +0100

    d09 - MirageMaintenance.kt placeholder

---
+++ src/y2023/d09/MirageMaintenance.kt
@@ -0,0 +1,22 @@
+package y2023.d09
+
+import y2023.PuzzleAnswer
+
+fun main() {
+    MirageMaintenance.checkPart1<Int, Int>()
+    MirageMaintenance.checkPart2<Int, Int>()
+}
+
+object MirageMaintenance : PuzzleAnswer<Int, Int> {
+    override val day = 9
+    override val expectedTestAnswerPart1 = 114
+    override val expectedTestAnswerPart2 = 0
+
+    override fun part1(input: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+}
